739e1802a29049c74949b5890c10dcfce1668972
Alexandr-Yermolov <dirynchik@gmail.com>
2021-08-08 16:58:32 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -18,13 +18,13 @@ const images = [
 const galleryContainer = document.querySelector('#gallery');
 const styleContainer = document.querySelector('style');
 
-const galeryMaker = images.map(image => {
-  return `<li class="gallery__box"><img class="galery__img" src="${image.url} alt="${image.alt} width = "300" height = "200"
+const galleryMaker = images.map(image => {
+  return `<li class="gallery__box"><img class="gallery__img" src="${image.url} alt="${image.alt} width = "300" height = "200"
 ></li>`
 })
 
-galleryContainer.insertAdjacentHTML('afterbegin', galeryMaker.join(''));
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMaker.join(''));
 
 styleContainer.insertAdjacentHTML('beforeend', `#gallery {display: flex; flex-wrap: wrap; justify-content: center;}
-  .galery__img {display: block;} .gallery__box{ list-style: none; margin: 15px;}`);
+  .gallery__img {display: block;} .gallery__box{ list-style: none; margin: 15px;}`);
 
